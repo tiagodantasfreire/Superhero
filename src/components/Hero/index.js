2923ceba093bc3f6c50c5969b66aa0e33da99549
Tiago Dantas <tiago.freire.2002@gmail.com>
2021-09-06 00:53:32 -0300

    toggle stats added

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,17 +2,79 @@ import React from "react";
 
 import * as S from "./styled";
 
-const Hero = ({ heroList }) => (
-  <>
-    {heroList.map(({ hero }) => {
-      return hero !== undefined && (
-        <S.HeroWrapper key={hero?.id}>
-          <S.HeroImage src={hero?.image?.url} alt={hero?.name} />
-          <S.HeroName>{hero?.name}</S.HeroName>
-        </S.HeroWrapper>
-      )
-    })}
-  </>
-);
+const Hero = ({ heroList }) =>  {
+  const toggleStats = (id) => {
+    const stats = document.getElementById(`${id}`).style;
+    stats.marginLeft === '-193px' ? stats.marginLeft = '-22px' : stats.marginLeft = '-193px';
+  };
+
+  return (
+    <>
+      {heroList.map(({ hero }) => {
+        return hero !== undefined && (
+          <S.HeroWrapper key={hero?.id}>
+            <S.HeroImageWrapper onClick={() => toggleStats(hero?.id)}>
+              <S.HeroImage src={hero?.image?.url} alt={hero?.name} />
+              <S.HeroName>{hero?.name}</S.HeroName>
+            </S.HeroImageWrapper>
+            <S.HeroStatsWrapper
+              id={hero.id}
+              style={{ marginLeft: '-193px' }}
+            >
+              <S.HeroStats>
+                <p>Combat:</p>
+                <S.PercentageWrapper>
+                  <span style={{ backgroundColor: '#ff2d2d', width: `${hero?.powerstats?.combat}%` }}>
+                    {hero?.powerstats?.combat}
+                  </span>
+                </S.PercentageWrapper>
+              </S.HeroStats>
+              <S.HeroStats>
+                <p>durability:</p>
+                <S.PercentageWrapper>
+                  <span style={{ backgroundColor: '#ffa500', width: `${hero?.powerstats?.durability}%` }}>
+                    {hero?.powerstats?.durability}
+                  </span>
+                </S.PercentageWrapper>
+              </S.HeroStats>
+              <S.HeroStats>
+                <p>intelligence:</p>
+                <S.PercentageWrapper>
+                  <span style={{ backgroundColor: '#4bb4da', width: `${hero?.powerstats?.intelligence}%` }}>
+                    {hero?.powerstats?.intelligence}
+                  </span>
+                </S.PercentageWrapper>
+              </S.HeroStats>
+              <S.HeroStats>
+                <p>power:</p>
+                <S.PercentageWrapper>
+                  <span style={{ backgroundColor: '#fdd700', width: `${hero?.powerstats?.power}%` }}>
+                    {hero?.powerstats?.power}
+                  </span>
+                </S.PercentageWrapper>
+              </S.HeroStats>
+              <S.HeroStats>
+                <p>speed:</p>
+                <S.PercentageWrapper>
+                  <span style={{ backgroundColor: '#861786', width: `${hero?.powerstats?.speed}%` }}>
+                    {hero?.powerstats?.speed}
+                  </span>
+                </S.PercentageWrapper>
+              </S.HeroStats>
+              <S.HeroStats>
+                <p>strength:</p>
+                <S.PercentageWrapper>
+                  <span style={{ backgroundColor: '#6b6b6b', width: `${hero?.powerstats?.strength}%` }}>
+                    {hero?.powerstats?.strength}
+                  </span>
+                </S.PercentageWrapper>
+              </S.HeroStats>
+            </S.HeroStatsWrapper>
+          </S.HeroWrapper>
+        )
+      })}
+    </>
+  );
+}
 
 export default Hero;
--- src/components/Hero/styled.js
+++ src/components/Hero/styled.js
@@ -2,19 +2,28 @@ import styled from 'styled-components';
 
 export const HeroWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: center;
   position: relative;
   cursor: pointer;
-  flex-wrap: wrap;
+  width: auto;
 `;
 
-export const HeroImage = styled.img`
-  height: 17rem;
+export const HeroImageWrapper = styled.div`
   width: 12rem;
-  object-fit: cover;
+  height: 17rem;
   border-radius: 1rem;
-`;
+  display: flex;
+  justify-content: center;
+  z-index: 1;
+  `;
+
+export const HeroImage = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  border-radius: 1rem;
+  border: 1px solid;
+  `;
 
 export const HeroName = styled.p`
   position: absolute;
@@ -26,4 +35,43 @@ export const HeroName = styled.p`
   font-family: 'VT323', monospace;
   text-transform: uppercase;
   font-size: 1.1rem;
+  `;
+
+export const HeroStatsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  padding: 0.5rem 1rem;
+  width: 10rem;
+  height: 16rem;
+  border: 1px solid;
+  border-radius: 1rem;
+  width: 10rem;
+  height: 16rem;
+  transition: margin 0.6s ease-in-out;
+  background: linear-gradient(#b3c5df 0%, #ebf4f5 100%) no-repeat;
+`;
+
+export const HeroStats = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  align-self: flex-end;
+`;
+
+export const PercentageWrapper = styled.div`
+  width: 100%;
+  height: 1rem;
+  border-radius: 1rem;
+  border: 1px solid;
+  background-color: #fff;
+  
+  span {
+    display: flex;
+    align-items: center;
+    padding-left: 5px;
+    height: 100%;
+    border-radius: 1rem;
+    max-width: calc(100% - 5px);
+  }
 `;
